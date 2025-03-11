b8b2b1c2d0b018b3dd1307bce740a29bc92416de
Erison Silva <hey@erison.work>
2025-03-11 14:07:15 -0300

    [AstroPaper][erison.work] local changes
    This commit contains changes in atropaper files, But it is necessary for
    my blog such as:
    
    - Modified about pagea
    - Remove old posts
    - Change initial text on index.php
    - Change color schema on style/base.css
    - Added umami track code on src/layouts/Layout.astro
    
    Added X social network
    
    Change Twitter to X
    
    Update dark theme
    
    Update local changes for astropaper 5
    
    Removed github files from astropaper
    
    Bump nodejs to 22.x
    
    Change readme
    
    Removed ci.yml file
    
    Local changes
    
    Use npm
    
    npm update after 5.3.0 upgrade
    
    Fix some changes from astro-paper
    
    - Layout
    - Config
    - Constant
    
    Remove ci.yaml file
    
    [AstroPaper] remove old posts
    
    Removed _releases and example blog folders
    
    Remove astro-paper articles

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 export const SITE = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
-  profile: "https://satnaing.dev/",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  website: "https://blog.erison.work",
+  profile: "https://blog.erison.work", // TODO: I do not know why we need this
+  author: "Erison Silva",
+  desc: "It's my personal blog, here I'm posting techinical posts and things that I consider intersting to share.",
+  title: "erison.work",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerIndex: 4,
@@ -14,10 +14,10 @@ export const SITE = {
   editPost: {
     enabled: true,
     text: "Edit page",
-    url: "https://github.com/satnaing/astro-paper/edit/main/",
+    url: "https://github.com/shield-wall/erison-work/edit/master/",
   },
   dynamicOgImage: true,
   dir: "ltr", // "rtl" | "auto"
   lang: "en", // html lang code. Set this empty and default will be "en"
-  timezone: "Asia/Bangkok", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+  timezone: "Europe/Berlin", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 } as const;
