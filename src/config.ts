dc7e0acd82d4e2c22bd4f8c54610a3fd7b81a4c2
Erison Silva <hey@erison.work>
2025-03-11 14:07:15 -0300

    [AstroPaper][erison.work] local changes
    This commit contains changes in atropaper files, But it is necessary for
    my blog such as:
    
    - Modified about pagea
    - Remove old posts
    - Change initial text on index.php
    - Change color schema on style/base.css
    - Added umami track code on src/layouts/Layout.astro
    
    Added X social network
    
    Change Twitter to X
    
    Update dark theme
    
    Update local changes for astropaper 5
    
    Removed github files from astropaper
    
    Bump nodejs to 22.x
    
    Change readme
    
    Removed ci.yml file

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 export const SITE = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
-  profile: "https://satnaing.dev/",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  website: "https://blog.erison.work",
+  profile: "https://blog.erison.work", // TODO: I do not know why we need this
+  author: "Erison Silva",
+  desc: "It's my personal blog, here I'm posting techinical posts and things that I consider intersting to share.",
+  title: "erison.work",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerIndex: 4,
@@ -12,7 +12,7 @@ export const SITE = {
   showArchives: true,
   showBackButton: true, // show back button in post detail
   editPost: {
-    url: "https://github.com/satnaing/astro-paper/edit/main/src/content/blog",
+    url: "https://github.com/shield-wall/erison-work/edit/master/src/data/blog",
     text: "Suggest Changes",
     appendFilePath: true,
   },
--- src/constants.ts
+++ src/constants.ts
@@ -5,8 +5,8 @@ import IconLinkedin from "@/assets/icons/IconLinkedin.svg";
 import IconWhatsapp from "@/assets/icons/IconWhatsapp.svg";
 import IconFacebook from "@/assets/icons/IconFacebook.svg";
 import IconTelegram from "@/assets/icons/IconTelegram.svg";
-import IconPinterest from "@/assets/icons/IconPinterest.svg";
 import { SITE } from "@/config";
+import type { GiscusProps } from "@giscus/react";
 
 export const LOCALE = {
   lang: "en", // html lang code. Set this empty and default will be "en"
@@ -16,25 +16,25 @@ export const LOCALE = {
 export const SOCIALS = [
   {
     name: "Github",
-    href: "https://github.com/satnaing/astro-paper",
+    href: "https://github.com/eerison",
     linkTitle: ` ${SITE.title} on Github`,
     icon: IconGitHub,
   },
   {
     name: "X",
-    href: "https://x.com/username",
+    href: "https://x.com/eerison",
     linkTitle: `${SITE.title} on X`,
     icon: IconBrandX,
   },
   {
     name: "LinkedIn",
-    href: "https://www.linkedin.com/in/username/",
+    href: "https://www.linkedin.com/in/eerison/",
     linkTitle: `${SITE.title} on LinkedIn`,
     icon: IconLinkedin,
   },
   {
     name: "Mail",
-    href: "mailto:yourmail@gmail.com",
+    href: "mailto:hey@erison.work",
     linkTitle: `Send an email to ${SITE.title}`,
     icon: IconMail,
   },
@@ -65,16 +65,17 @@ export const SHARE_LINKS = [
     linkTitle: `Share this post via Telegram`,
     icon: IconTelegram,
   },
-  {
-    name: "Pinterest",
-    href: "https://pinterest.com/pin/create/button/?url=",
-    linkTitle: `Share this post on Pinterest`,
-    icon: IconPinterest,
-  },
-  {
-    name: "Mail",
-    href: "mailto:?subject=See%20this%20post&body=",
-    linkTitle: `Share this post via email`,
-    icon: IconMail,
-  },
 ] as const;
+
+export const GISCUS: GiscusProps = {
+  repo: "shield-wall/erison-work",
+  repoId: "R_kgDOJuBh5Q",
+  category: "Posts",
+  categoryId: "DIC_kwDOJuBh5c4CixZR",
+  mapping: "pathname",
+  reactionsEnabled: "1",
+  emitMetadata: "0",
+  inputPosition: "bottom",
+  lang: "en",
+  loading: "lazy",
+};
